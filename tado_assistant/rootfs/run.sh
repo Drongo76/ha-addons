282011b0f62dc68de2592de161f8bad65c3754b9
Drongo76 <61839601+Drongo76@users.noreply.github.com>
2025-12-26 17:53:55 +0100

    Update run.sh

--- tado_assistant/rootfs/run.sh
+++ tado_assistant/rootfs/run.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/with-contenv bashio
 set -e
 
+bashio::log.info "Starting Tado Assistant (Ingress) - repo build v0.3.0 (MARKER: 2025-12-26-A)"
+
 export FLASK_ENV=production
 export FLASK_DEBUG=0
 export WERKZEUG_DEBUG_PIN=off
 export PORT="${PORT:-8099}"
 
-exec python3 /app.py
+exec python3 -u /app.py
