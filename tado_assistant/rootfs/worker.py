5d68a54d4ea12f48329ed53e383263d305725e7f
Drongo76 <61839601+Drongo76@users.noreply.github.com>
2025-12-31 01:38:19 +0100

    Update worker.py

--- tado_assistant/rootfs/worker.py
+++ tado_assistant/rootfs/worker.py
@@ -728,14 +728,6 @@ def publish_discovery_for_devices(
             continue
         did_int = int(did)
 
-        mobile_device_block = {
-            "identifiers": [f"{ha_device_id}_home_{home_id}_mobile_{did_int}"],
-            "name": f"{name}",
-            "manufacturer": "tado°",
-            "model": "Mobile Device",
-            "via_device": ha_device_id,
-        }
-
         tracker_object_id, raw_object_id, old_json_object_id = discovery_object_ids(ha_device_id, did_int)
 
         state_topic = f"{topic_prefix}/presence/home_{home_id}/device_{did_int}/state"
@@ -751,7 +743,8 @@ def publish_discovery_for_devices(
             "payload_home": "home",
             "payload_not_home": "not_home",
             "source_type": "gps",
-            "device": mobile_device_block,
+            "entity_category": "config",
+            "device": main_device_block,
         }
         mpub.publish_json(tracker_config_topic, tracker_payload, retain=True)
 
@@ -768,7 +761,7 @@ def publish_discovery_for_devices(
 "entity_category": "diagnostic",
 "enabled_by_default": False,
 "icon": "mdi:clipboard-text-outline",
-"device": mobile_device_block,
+"device": main_device_block,
             }
             mpub.publish_json(raw_config_topic, raw_payload, retain=True)
 
